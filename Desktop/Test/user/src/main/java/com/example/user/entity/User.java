edeb6df785c5af5706de56005ae63078c6260faf
rubanin <rubanraj.inbaraj@gmail.com>
2022-02-27 23:14:28 +0530

    Commit changes

--- Desktop/Test/user/src/main/java/com/example/user/entity/User.java
+++ Desktop/Test/user/src/main/java/com/example/user/entity/User.java
@@ -2,7 +2,6 @@ package com.example.user.entity;
 
 
 import java.sql.Date;
-import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,18 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="User",schema="ruban")
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -50,15 +40,18 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	private String email;
-	private String userName;
-	private String password;
-//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private Date lastLogin;
+	public Date getLastLogin() {
+		return lastLogin;
+	}
 	public void setLastLogin(Date lastLogin) {
 		this.lastLogin = lastLogin;
 	}
+	
+	private String email;
+	private String userName;
+	private String password;
+	private Date lastLogin;
+	
 	
 
 	
